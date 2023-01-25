1636c7de5f3e8a634105d40db799a870809614f7
Chris <98065905+korozos@users.noreply.github.com>
2023-01-25 23:39:27 +0000

    updated ui for rps game; deleted getWeapon functions for user and CPU

--- rps.js
+++ rps.js
@@ -1,30 +1,19 @@
 
 const weapons = ['Rock', 'Paper', 'Scissors'];
-const randomWeapons = Math.floor((Math.random()* weapons.length))
+const randomWeapon = Math.floor((Math.random()* weapons.length))
 
 
-// Get Choices
-function getComputerChoice(){
-    let computerChoice = randomWeapons;
-    return computerChoice
-   
-}
 
-function getPlayerChoice(){
-    let playerChoice = prompt('Rock, paper, or scissors?')
-    (playerChoice == 'Rock')? 0: (playerChoice == 'Paper')? 1: (playerChoice == 'scissors')? 2: undefined;
-    return playerChoice
-}
+
+
 
 
 
 
 
 // Game Function
-function playGame(roundCountPref){
+function playGame(roundCountPref, ){
     for (let i = 0; i <= (roundCountPref); i++){
-        getComputerChoice();
-        getPlayerChoice();
         if (playerChoice == undefined){
             console.log('Please type rock, paper, or scissors')
         }
@@ -32,4 +21,11 @@ function playGame(roundCountPref){
 }
 
 
+const body = document.body;
+const rockButton = document.getElementById("rockButton");
+const paperButton = document.getElementById("paperButton");
+const scissorsButton = document.getElementById("scissorsButton");
 
+rockButton.addEventListener("click", () => {
+  
+})
