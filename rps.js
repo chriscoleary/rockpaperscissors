b10d40d4a84132a3f6ea8184a5ff76e19349dc6e
Chris <98065905+korozos@users.noreply.github.com>
2023-01-26 03:53:25 +0000

    updated game ui/scoreboard.

--- rps.js
+++ rps.js
@@ -1,31 +1,65 @@
 
 const weapons = ['Rock', 'Paper', 'Scissors'];
-const randomWeapon = Math.floor((Math.random()* weapons.length))
 
 
 
-
-
-
-
-
-
-
-// Game Function
-function playGame(roundCountPref, ){
-    for (let i = 0; i <= (roundCountPref); i++){
-        if (playerChoice == undefined){
-            console.log('Please type rock, paper, or scissors')
-        }
-    }
+function getComputerChoice(){
+  const randomWeapon = Math.floor(Math.random()* weapons.length);
+  return weapons[randomWeapon];
 }
 
-
+let computerWs = 0;
+let userWs = 0;
 const body = document.body;
 const rockButton = document.getElementById("rockButton");
 const paperButton = document.getElementById("paperButton");
 const scissorsButton = document.getElementById("scissorsButton");
+const div = document.getElementById("results");
+
+
+const scoreboard = document.createElement("h3");
+scoreboard.textContent = `User ${userWs} - ${computerWs} Computer`;
+div.appendChild(scoreboard);
+const result = document.createElement("p");
+result.textContent = "";
+div.appendChild(result);
+
+
+function adjGame(){
+  if (userWs==5){
+    result.textContent = "You win!";
+    userWs = 0;
+    computerWs = 0;
+  } else if (computerWs==5){
+    result.textContent = "The computer won";
+    userWs=0;
+    computerWs=0;
+  }
+}
+
+
+
+
 
 rockButton.addEventListener("click", () => {
-  
-})
+  const userChoice = 'Rock';
+  let computerChoice = getComputerChoice();
+  if (computerChoice=='Scissors'){
+    userWs += 1;
+    scoreboard.textContent = `User ${userWs} - ${computerWs} Computer`;
+    result.textContent = "Rock beats Scissors!";
+    adjGame();
+    
+  } else if (computerChoice=='Rock'){
+    computerChoice=getComputerChoice();
+  } else {
+    computerWs += 1;
+    scoreboard.textContent = `User ${userWs} - ${computerWs} Computer`;
+    result.textContent = "Paper beats Rock!";
+    adjGame();
+    
+  }
+});
+
+
+
